917126e54db73831ed82f7300ed04d1afbcc00c1
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-17 13:08:19 +0200

    Upload header

---
+++ travel-journal/src/components/JournalHeader.jsx
@@ -0,0 +1,8 @@
+export default function JournalHeader() {
+  return(
+    <header className="journal-header">
+      <img src="images/globe.png" alt="globe img" />
+      <span>My travel journal.</span>
+    </header>
+  )
+}
---
+++ travel-journal/src/components/JournalMainContent.jsx
@@ -0,0 +1,17 @@
+export default function JournalMain() {
+  return(
+    <main className="journal-main">
+      <div>
+        <div>
+        <img src="" alt="Japan" />
+        </div>
+        <div>
+          <img src="images/marker.png" alt="marker" />
+        </div>
+        
+
+      </div>
+      
+    </main>
+  )
+}
