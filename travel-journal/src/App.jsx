1d81bc41e8dd9423127719870e7fc4c32bd73281
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-18 19:04:26 +0200

    pass object as prop

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,28 +1,19 @@
 import JournalHeader from "./components/JournalHeader.jsx";
 import Entry from "./components/JournalMainContent.jsx";
 import travelData from '/src/data/data.js';
-console.log(travelData);
-
 
 export default function App() {
 
   const entryData = travelData.map(destination => {
-  const {coutry, dates, googleMapsLink, img, text, title} = destination;
+  const {coutry, dates, googleMapsLink, img, text, title, id} = destination;
     return (
       <Entry 
-         img={{
-            src: img.src,
-            alt: img.alt
-         }}
-         title= {title}
-         country= {coutry}
-         googleMapsLink= {googleMapsLink}
-         dates= {dates}
-         text= {text}
+         key={id}
+         entry= {destination}
       />
     )
   })
-
+  console.log(entryData);
   return (
     <>
       <JournalHeader />
