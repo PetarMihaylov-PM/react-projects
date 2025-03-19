3c8ddb1ba226c5a7fa187290988d48064a2bdc1f
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-19 17:50:20 +0200

    adding functionality

--- bite-builder/src/components/MainContent.jsx
+++ bite-builder/src/components/MainContent.jsx
@@ -1,18 +1,35 @@
 export default function MainContent() {
+  const ingredients = ['chicken', 'tomatoes', 'Oregano'];
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log('submitted');
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get('ingredient'); // using the name key of the form;
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
+  const mapIngredients = ingredients.map(element => {
+    return(
+      <li key={element}> {element} </li>
+    )
+  });
+
   return(
     <>
-      <main className="mainContent">
+      <form className="mainContent" onSubmit={handleSubmit}>
         <div className="inputAndButton">
-          <input type="text" placeholder="Type your ingredient here"/>
+          <input type="text" placeholder="e.g oregano" aria-label="Add ingredient" name="ingredient"/>
           <button>+ Add ingredient </button>
         </div>
         <div className="ingredients-container">
           <h1>Ingredients on hand:</h1>
           <ul>
-            <li>Chicken breasts</li>
+            {mapIngredients}
           </ul>
         </div>
-      </main>
+      </form>
     </> 
   )
 }
