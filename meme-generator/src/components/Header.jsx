a746f60b8e476765cc1e99862994b9a2262a559b
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-03-26 15:26:02 +0200

    Creating the header component

---
+++ meme-generator/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="/src/assets/troll-face.png" alt="troll-face" />
+      <span>Meme Generator</span>
+    </header>
+  )
+}
