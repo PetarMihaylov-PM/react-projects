489c1e1d0c910962439495bf519164418874d69f
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-01 22:40:23 +0300

    creating a function to generate our 10 random dice numbers

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import Die from "./components/Die";
 
+
+function generateAllDice() {
+  return new Array(10)
+    .fill(0)
+    .map(() => Math.ceil(Math.random() * 6));
+}
+
+console.log(generateAllDice())
+
+
+
 export default function App(){
   return(
     <>
       <main className="mainContent">
         <div className="dice-container">
+          
           <Die value={1}/>
           <Die value={2}/>
           <Die value={3}/>
           <Die value={4}/>
           <Die value={5}/>
           <Die value={6}/>
-          <Die value={7}/>
-          <Die value={8}/>
-          <Die value={9}/>
-          <Die value={10}/>
+          <Die value={5}/>
+          <Die value={2}/>
+          <Die value={5}/>
+          <Die value={6}/>
         </div>
         
       </main>
