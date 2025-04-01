d419e32512cae13abe849f7769c598f872a555e2
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-01 22:47:42 +0300

    generating the dice components with .map()

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -2,32 +2,23 @@ import React from "react";
 import Die from "./components/Die";
 
 
-function generateAllDice() {
-  return new Array(10)
-    .fill(0)
-    .map(() => Math.ceil(Math.random() * 6));
-}
-
-console.log(generateAllDice())
-
-
-
 export default function App(){
+
+  const [dice, setDice] = React.useState(generateAllDice());
+
+  function generateAllDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6));
+  }
+
+  const diceElements = dice.map(num => <Die value={num}/>)
+  
   return(
     <>
       <main className="mainContent">
         <div className="dice-container">
-          
-          <Die value={1}/>
-          <Die value={2}/>
-          <Die value={3}/>
-          <Die value={4}/>
-          <Die value={5}/>
-          <Die value={6}/>
-          <Die value={5}/>
-          <Die value={2}/>
-          <Die value={5}/>
-          <Die value={6}/>
+          {diceElements}
         </div>
         
       </main>
