f3ef7aaeb4a489e479a5122d6bfeca4e75f3d43d
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-05 22:41:41 +0300

    initial commit

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Die from "./components/Die";
 import { nanoid } from 'nanoid';
-import Confetti from 'react-confetti'
+import Confetti from 'react-confetti';
 
 
 export default function App(){
