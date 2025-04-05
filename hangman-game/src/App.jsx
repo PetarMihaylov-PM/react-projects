9740ce8a69c6e5e76f66b2c81089a801db3503c9
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-05 23:22:08 +0300

    creating the header element

--- hangman-game/src/App.jsx
+++ hangman-game/src/App.jsx
@@ -1,3 +1,5 @@
+import Main from "./components/Main"
+
 export default function App() {
   return (
     <>
---
+++ hangman-game/src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Main() {
+  return(
+    <main>
+      <header className="header-container">
+        <h1>HangMan: Dev edition</h1>
+        <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+    </main>
+  )
+}
