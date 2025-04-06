313fe2d73743072cbdbc1693282a1a43818f9f72
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-06 22:35:04 +0300

    adding elements and style

--- hangman-game/src/components/Main.jsx
+++ hangman-game/src/components/Main.jsx
@@ -8,6 +8,22 @@ export default function Main() {
         <h1>HangMan: Dev edition</h1>
         <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
+      <section className="status-bar">
+          <h2>You win!</h2>
+          <p>Well Done!</p>
+      </section>
+      <section className="chips">
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      
+      </section>
     </main>
   )
 }
