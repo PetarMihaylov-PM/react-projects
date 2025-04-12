bfb5e98ba23ef7e5c5bf052a43b06fa960e1aec6
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-12 14:11:50 +0300

    adding states and updating the newTask onChange

--- to-do-app/src/components/MainContent.jsx
+++ to-do-app/src/components/MainContent.jsx
@@ -1,7 +1,28 @@
+import React from "react"
+
 export default function MainContent() {
+
+const [tasks, setTasks] = React.useState([]);
+const [newTask, setNewTask] = React.useState('');
+
+function handleChange(event) {
+  setNewTask(event.currentTarget.value);
+  console.log(newTask);
+}
+
   return(
     <section>
-      hello
+      <div className="input-group">
+        <input 
+          type="text" 
+          placeholder="Write your task here"
+          value={newTask}
+          onChange={handleChange}
+        />
+        <button onClick={addTask}>+</button>
+      </div>
+
+
     </section>
   )
 }
