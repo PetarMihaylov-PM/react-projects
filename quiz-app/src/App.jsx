d1bb71f0367edcc58c7013feb207e7808efccd2a
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-14 23:23:16 +0300

    fetching questions and making answers interactive

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,4 +1,49 @@
+import React from "react"
+
 export default function App() {
+
+const [questionsData, setQuestionsData] = React.useState([]);
+const [currentIndex, setCurrentIndex] = React.useState(0);
+const [score, setScore] = React.useState(0);
+const [selectedAnswer, setSelectedAnswer] = React.useState(null);
+
+const currentQuestion = questionsData[currentIndex];
+
+React.useEffect(() => {
+  async function getQuiestions() {
+    const data = 
+      await fetch("https://opentdb.com/api.php?amount=10&type=multiple").
+      then(response => response.json());
+
+      const processedData = data.results.map(question => ({
+        ...question,
+        all_answers: shuffleAnswers(question.correct_answer, question.incorrect_answers)
+      }))
+      
+      setQuestionsData(processedData); 
+  }
+
+  getQuiestions();
+}, []);
+
+function shuffleAnswers(correct, incorrect) {
+  const combineAnswers = [...incorrect, correct];
+  return combineAnswers.sort(() => Math.random() - 0.5);
+}
+
+function decodeHtml(html) {
+  const txt = document.createElement('textarea');
+  txt.innerHTML = html;
+  return txt.value;
+}
+
+function handleAnswerClick(answer) {
+  setSelectedAnswer(answer)
+  if(answer === currentQuestion.correct_answer){
+    setScore(prev => prev + 1);
+  }
+}
+
   return (
     <div className="quiz-app-container">
       <header>
@@ -8,21 +53,38 @@ export default function App() {
           <span>20</span>
         </div>
       </header>
-      <section className="questions-container">
-        <h3>The question will go here</h3>
-        <button>
-          <p>True</p>
-        </button>
-        <button>
-          <p>False</p>
-        </button>
-        <button>
-          <p>Sometimes</p>
-        </button>
-        <button>
-          <p>Everytime</p>
-        </button>
-      </section>
+      { questionsData.length > 0 ? 
+        (<section className="questions-container">
+          <h3>{decodeHtml(currentQuestion.question)}</h3>
+          <div className="answers">
+            {
+              currentQuestion.all_answers.map((answer, i) => (
+                <button 
+                  key={i}
+                  className={
+                    selectedAnswer
+                      ? answer === currentQuestion.correct_answer
+                        ? 'correct'
+                        : answer === selectedAnswer
+                        ? 'incorrect'
+                        : 'disabled'
+                      : 'answer-button'
+                  }
+                  onClick={() => handleAnswerClick(answer)}
+                  disabled={selectedAnswer !== null}
+                >
+                  {decodeHtml(answer)}
+                </button>
+              ))
+            }
+          </div>
+          <div>
+            <span>Score: {score}</span>
+          </div>
+        </section>) 
+        : 
+        null
+      }
     </div>
   )
 }
