c67b18639d643335bc06f878dbc9a024fd7dd9b3
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-18 22:17:54 +0300

    styling

--- restaurant-website/src/components/Navbar.jsx
+++ restaurant-website/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Navbar() {
   return(
     <nav className="nav-bar-container">
-      <h3>Pete's restaurant</h3>
+      <h3 className="restaurant-name">Pete's restaurant</h3>
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/menu'>Menu</Link></li>
