1eb2b91cceae2e55f845d7fac72fdfa4b503875b
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-19 21:53:03 +0300

    styling

--- restaurant-website/src/components/Footer.jsx
+++ restaurant-website/src/components/Footer.jsx
@@ -1,5 +1,9 @@
 export default function Footer() {
   return(
-    <h1>footer</h1>
+    <div className="footer-container">
+      <img src="https://freepnglogo.com/images/all_img/1730342312_facebook-icon-png.png" alt="fb" />
+      <img src="https://static-00.iconduck.com/assets.00/instagram-fill-icon-2048x2048-129c4vdo.png" alt="insta" />
+      <img src="https://cdn-icons-png.flaticon.com/512/665/665228.png" alt="twitter" />
+    </div>
   )
 }
--- restaurant-website/src/pages/Home.jsx
+++ restaurant-website/src/pages/Home.jsx
@@ -4,6 +4,7 @@ export default function Home(){
     <div className="home-container">
       <h3>Hungry for food? Or just good vibes?</h3>
       <h4>Either way, you’ve come to the right plate.</h4>
+      <button className="menu-button">Menu</button>
     </div>
       
     </>
