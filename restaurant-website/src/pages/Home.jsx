7f1ab848cb042647748a04808c525b0b5a853583
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-19 23:11:25 +0300

    Adding content and styling the about page

--- restaurant-website/src/pages/Home.jsx
+++ restaurant-website/src/pages/Home.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Home(){
+
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/menu');
+  }
+
   return(
     <>
     <div className="home-container">
       <h3>Hungry for food? Or just good vibes?</h3>
       <h4>Either way, you’ve come to the right plate.</h4>
-      <button className="menu-button">Menu</button>
+      <button className="menu-button" onClick={handleClick}>Menu</button>
     </div>
     </>
   )
