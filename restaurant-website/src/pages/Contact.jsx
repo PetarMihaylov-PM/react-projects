a1704c27dd042f57991dc7fff0d11891d36cd212
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-20 19:24:57 +0300

    Update Contact.jsx

--- restaurant-website/src/pages/Contact.jsx
+++ restaurant-website/src/pages/Contact.jsx
@@ -1,7 +1,29 @@
 export default function Contact(){
   return(
     <>
-      <h1>contact</h1>
+      <div className="contact-page">
+        <h1>
+          We’d love to hear from you! Whether you’re looking to reserve a table, inquire about catering, or just say hi — feel free to reach out.
+        </h1>
+        <div className="contact-information">
+          <div className="address-info">
+            <h2>Address:</h2>
+            <h2>
+              Pete’s Restaurant <br />
+              123 Flavor Street <br />
+              Culinary Town, FL 45678
+            </h2>
+          </div>
+          <div className="phone-info">
+            <h2>Phone:</h2>
+            <h2>
+              (123) 456-7890
+            </h2>
+          </div>
+        </div>
+      </div>
+     
+
     </>
   )
 }
