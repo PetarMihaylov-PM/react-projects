37124ffb42a64a6ff8454f4f88fee280ac210810
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-24 11:29:33 +0300

    Cart page styling

--- restaurant-website/src/pages/Cart.jsx
+++ restaurant-website/src/pages/Cart.jsx
@@ -9,24 +9,33 @@ export default function Cart() {
   const renderCartItems = cartItems.map((item, index) => 
       <div 
       key={index}
-      className="cart-item">
-        <img src={item.image} alt="food pic" />
-        <h3>{item.name}</h3>
-        <h3>{item.price}</h3>
+      className="cart-item-card">
+        <img 
+          className="food-img"
+          src={item.image} 
+          alt="food pic" />
+        <h3 
+          className="cart-item-name"
+        >
+          {item.name}
+        </h3>
+        <h3 className="cart-item-price"
+        >
+          {item.price}
+        </h3>
         <input type="number" />
-        <img src={deleteIcon} alt="delete-icon" />
+        <img 
+          className="delete-icon"
+          src={deleteIcon} alt="delete-icon" />
       </div>
   );
-
-  console.log(cartItems)
-  console.log(renderCartItems)
 
   return(
     <div className="cart-container">
       <header>
         <h1>Your <span>Cart</span></h1>
       </header>
-      <main>
+      <main className="orders-cart-container">
         {cartItems.length > 0 ? 
           renderCartItems
           :
