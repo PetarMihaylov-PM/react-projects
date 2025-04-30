e3b1fe5f212d845374c950ada7bbbbcd80da7ab9
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-30 22:08:48 +0300

    updating the Checkout form

--- restaurant-website/src/pages/Checkout.jsx
+++ restaurant-website/src/pages/Checkout.jsx
@@ -19,6 +19,41 @@ export default function Checkout() {
           <h4>Street Address <span>required</span></h4>
           <input type="text" />
         </div>
+        <div className="city-input-container">
+          <div>
+            <h4>Postal Code <span>required</span></h4>
+            <input type="number" />
+          </div>
+          <div>
+            <h4>Town/City <span>required</span></h4>
+            <input type="text" />
+          </div>
+        </div>
+        <div className="phone-number-container">
+          <h4>Phone Number <span>required</span></h4>
+          <input type="number" placeholder=" +359 ..."/>
+        </div>
+        <div className="payment-type-container">
+          <h4>Payment Type <span>required</span></h4>
+          <div className="payment-type-radio-btns">
+            <div>
+              <input 
+                type="radio"
+                id='cash'
+                name="payment-type"
+              />
+              <label htmlFor="cash">Cash on arrival</label>
+            </div>
+            <div>
+              <input 
+                type="radio"
+                id='card' 
+                name="payment-type"
+              />
+              <label htmlFor="card">Card</label>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="checkout-right">
         <div className="checkout-right-header">
