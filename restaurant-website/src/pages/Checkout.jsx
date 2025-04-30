c1b41ee30b7138daec5f7a83f79c439db36a2df7
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-04-30 22:23:02 +0300

    creating the card payment section

--- restaurant-website/src/pages/Checkout.jsx
+++ restaurant-website/src/pages/Checkout.jsx
@@ -1,3 +1,5 @@
+import creditCard from '../assets/credit-card.png'
+
 
 export default function Checkout() {
   return (
@@ -5,20 +7,24 @@ export default function Checkout() {
       <div className="checkout-left">
         <h1>Checkout</h1>
         <h3>Ship To</h3>
+
         <div className="name-input-container">
           <div className="first-name-container">
             <h4>First Name <span>required</span></h4>
             <input type="text" />
           </div>
+
           <div className="last-name-container">
             <h4>Last Name <span>required</span></h4>
             <input type="text" />
           </div>
         </div>
+
         <div className="street-address-container">
           <h4>Street Address <span>required</span></h4>
           <input type="text" />
         </div>
+
         <div className="city-input-container">
           <div>
             <h4>Postal Code <span>required</span></h4>
@@ -29,10 +35,12 @@ export default function Checkout() {
             <input type="text" />
           </div>
         </div>
+
         <div className="phone-number-container">
           <h4>Phone Number <span>required</span></h4>
           <input type="number" placeholder=" +359 ..."/>
         </div>
+
         <div className="payment-type-container">
           <h4>Payment Type <span>required</span></h4>
           <div className="payment-type-radio-btns">
@@ -42,7 +50,7 @@ export default function Checkout() {
                 id='cash'
                 name="payment-type"
               />
-              <label htmlFor="cash">Cash on arrival</label>
+              <label htmlFor="cash">Cash <span>on arrival</span></label>
             </div>
             <div>
               <input 
@@ -51,6 +59,21 @@ export default function Checkout() {
                 name="payment-type"
               />
               <label htmlFor="card">Card</label>
+            </div>
+          </div>
+
+          <div className="card-payment-container">
+            <img src={creditCard} alt="credit-card-img" />
+            <input type="text" placeholder="CARD NUMBER"/>
+            <input type="text" placeholder="CARDHOLDER NAME"/>
+            <div className="card-info">
+              <input type="text" placeholder="MM"/>
+              <input type="text" placeholder="YY"/>
+              <input type="text" placeholder="CVV"/>
+            </div>
+            <div className="card-buttons">
+              <button className="pay-btn">PAY NOW</button>
+              <button className="cancel-btn">CANCEL</button>
             </div>
           </div>
         </div>
