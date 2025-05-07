af09c50a353c9a860d7850e482a5524835d548f7
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-07 14:42:00 +0300

    sidebar setup

--- google-gemini-app/src/App.jsx
+++ google-gemini-app/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 export default function App() {
   return(
-    <div>
-      
-    </div>
+    <>
+      <Sidebar />
+    </>
   )
 }
