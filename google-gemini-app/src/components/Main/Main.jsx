657a5e1ea5d215b7eec9a3e925d39a640401199b
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-08 16:32:14 +0300

    Main setup

--- google-gemini-app/src/components/Main/Main.jsx
+++ google-gemini-app/src/components/Main/Main.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import './Main.css';
 import { assets } from '../../assets/assets.js';
 
-export const Main = () => {
+const Main = () => {
   return (
     <div className='main'>
       <div className="nav">
         <p>Gemini</p>
         <img src={assets.user_icon} alt="user_icon" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p><span>Hello, Peter.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default Main
