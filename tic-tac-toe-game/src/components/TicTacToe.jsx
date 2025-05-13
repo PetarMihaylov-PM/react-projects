da8744cce235293b1d58979cd6f0e9a136f9fd41
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-13 21:25:34 +0300

    working on the game logic

--- tic-tac-toe-game/src/components/TicTacToe.jsx
+++ tic-tac-toe-game/src/components/TicTacToe.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TicTacToe.css';
 import circle_icon from '../assets/circle.png'
 import cross_icon from '../assets/cross.png';
 
+const winningPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 export const TicTacToe = () => {
+
+  const [board, setBoard] = useState(Array(9).fill(''));
+  const [isXTurn, setIsXTurn] = useState(true);
+  const [winner, setWinner] = useState(null);
+
+  function checkWinner(newBoard) {
+    for (let pattern of winningPattern) {
+      const [a, b, c] = pattern;
+      if(
+        newBoard[a] &&
+        newBoard[a] === newBoard[b] && 
+        newBoard[a] === newBoard[c]
+      ) {
+        return newBoard[a];
+      }
+    }
+    return null;
+  };
+
+  function handleClick(index) {
+    if(board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = isXTurn ? 'X' : 'O';
+  }
+
   return (
     <div className='container'>
       <h1 className="title">Tic Tac Toe Game in <span>React</span></h1>
