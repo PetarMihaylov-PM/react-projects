9bb0e821a80ef33623351002faece326f21e86c4
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-13 20:59:04 +0300

    project setup

---
+++ tic-tac-toe-game/src/components/TicTacToe.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TicTacToe.css';
+import circle_icon from '../assets/cirle.png';
+import cross_icon from '../assets/cross.png';
+
+export const TicTacToe = () => {
+  return (
+    <div></div>
+  )
+}
