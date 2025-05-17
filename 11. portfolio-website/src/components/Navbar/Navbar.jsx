ada4de71ea1da3043f10716ddfabe66a8a48f815
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-17 13:35:36 +0300

    styling the navbar elements

--- 11. portfolio-website/src/components/Navbar/Navbar.jsx
+++ 11. portfolio-website/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
+import contactImg from '../../assets/contact.png'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="nav-bar">
       <img src={logo} alt="logo" className='logo'/>
-      <div className="desktop-menu">
-        <Link className="desktop-menu-list-item" to='/'>Home</Link>
-        <Link className="desktop-menu-list-item" to='/about'>About</Link>
-        <Link className="desktop-menu-list-item" to='/portfolio'>Portfolio</Link>
-        <Link className="desktop-menu-list-item" to='/clients'>Clients</Link>
+      <div className="navbar-menu">
+        <Link className="navbar-menu-list-item" to='/'>Home</Link>
+        <Link className="navbar-menu-list-item" to='/about'>About</Link>
+        <Link className="navbar-menu-list-item" to='/portfolio'>Portfolio</Link>
+        <Link className="navbar-menu-list-item" to='/clients'>Clients</Link>
       </div>
-      <button className="desktop-menu-btn">
-        <img src="" alt="" className="dektop-icon" />
+      <button className="navbar-menu-btn">
+        <img src={contactImg} alt="contact-icon" className="dektop-icon" />
         Contact me
       </button>
     </nav>
