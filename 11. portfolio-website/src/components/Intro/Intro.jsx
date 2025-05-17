21bdfc3aa8a821c174235bd47507f4a55dacc82e
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-17 15:19:40 +0300

    styling for mobile phones

--- 11. portfolio-website/src/components/Intro/Intro.jsx
+++ 11. portfolio-website/src/components/Intro/Intro.jsx
@@ -10,8 +10,13 @@ const Intro = () => {
       <div className="intro-content">
         <span className='hello'>Hello,</span>
         <span className='intro-text'>I'm <span className='intro-name'>Kermit</span> <br /> Website Designer</span>
-        <p>I'm a web designer skilled in creating responsive, visually appealing websites using modern frameworks like React.</p>
-        <Link><button className="hire-button"><img src={hireIcon} alt="hire-me" />Hire me</button></Link>
+        <p>I'm a web designer skilled in creating responsive, visually appealing websites <br /> using modern frameworks like React.</p>
+        <button className="hire-button">
+          <img src={hireIcon} alt="hire-me" />
+          <p>
+            Hire me
+          </p>
+        </button>
       </div>
       <img src={personImg} alt="person-img" className="person-img" />
     </section>
