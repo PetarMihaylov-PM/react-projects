a05bc4e1a2b81381765d7bdf520d7807c4a24e33
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-17 13:23:35 +0300

    creating routes for the pages in the navbar

--- 11. portfolio-website/src/App.jsx
+++ 11. portfolio-website/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
     <div className='App'>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/'/>
+          <Route path='/about'/>
+          <Route path='/portfolio'/>
+          <Route path='/clients'/>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- 11. portfolio-website/src/components/Navbar/Navbar.jsx
+++ 11. portfolio-website/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Navbar.css';
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="nav-bar">
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" className='logo'/>
       <div className="desktop-menu">
-
+        <Link className="desktop-menu-list-item" to='/'>Home</Link>
+        <Link className="desktop-menu-list-item" to='/about'>About</Link>
+        <Link className="desktop-menu-list-item" to='/portfolio'>Portfolio</Link>
+        <Link className="desktop-menu-list-item" to='/clients'>Clients</Link>
       </div>
       <button className="desktop-menu-btn">
         <img src="" alt="" className="dektop-icon" />
