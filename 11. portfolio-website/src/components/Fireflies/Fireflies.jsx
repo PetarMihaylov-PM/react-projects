27e8030568b627a540bb8ba708f40814fb90fba6
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-26 20:20:25 +0300

    adding more fireflies(dots) to the background and making them move in different patterns

--- 11. portfolio-website/src/components/Fireflies/Fireflies.jsx
+++ 11. portfolio-website/src/components/Fireflies/Fireflies.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Fireflies.css';
 
+const getRandom = (min, max) => Math.random() * (max - min) + min;
+
 const Fireflies = () => {
-  const fireflyCount = 20;
-  
+  const fireflyCount = 50;
+
   return (
-    <div className='fireflies'>
-      {[...Array(fireflyCount)].map((_, i) => (
-        <div className="firefly" key={i} />
-      ))}
+    <div className="fireflies">
+      {[...Array(fireflyCount)].map((_, i) => {
+        const style = {
+          top: `${getRandom(0, 100)}vh`,
+          left: `${getRandom(0, 100)}vw`,
+          animationDuration: `${getRandom(10, 30)}s`,
+          animationDelay: `${getRandom(0, 10)}s`,
+        };
+
+        return <div className="firefly" style={style} key={i} />;
+      })}
     </div>
   );
 };
