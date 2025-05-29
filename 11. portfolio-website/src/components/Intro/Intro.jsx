2a5eacb94d0c48d8bffd69a315eed4a5de449ab0
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-05-29 11:00:09 +0300

    Update Intro.jsx

--- 11. portfolio-website/src/components/Intro/Intro.jsx
+++ 11. portfolio-website/src/components/Intro/Intro.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Intro.css';
+import { Link } from 'react-scroll';
 import personImg from '../../assets/profile-img.png';
-import hireIcon from '../../assets/hireme.png'
+import hireIcon from '../../assets/hireme.png';
+
 
 const Intro = () => {
   return (
@@ -12,17 +14,21 @@ const Intro = () => {
           I'm <span className='intro-name'>Petar Mihaylov</span> <br /> Frontend Developer
         </span>
         <p>I'm a frontend developer skilled in creating responsive, visually appealing websites <br /> using modern frameworks like React.</p>
-        <button 
-          className="hire-button"
-          onClick={() => {
-            document.getElementById('contact-page').  scrollIntoView({behavior: 'smooth'});
-          }}>
-          <img src={hireIcon} alt="hire-me" 
-            />
-          <p>
-            Hire me
-          </p>
-        </button>
+        <Link 
+          to='contact-page'
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+            <button className="hire-button">
+              <img src={hireIcon} alt="hire-me" 
+                />
+              <p>
+                Hire me
+              </p>
+            </button>
+        </Link>
+      
       </div>
       <img src={personImg} alt="person-img" className="person-img" />
     </section>
